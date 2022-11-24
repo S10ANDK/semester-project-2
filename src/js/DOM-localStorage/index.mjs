28d4d91610ed44636a6f2a74e2e24c7a36f90b1a
S10ANDK <91675991+S10ANDK@users.noreply.github.com>
2022-11-24 19:26:24 +0100

    added folder and file structure for JS

---
+++ src/js/DOM-localStorage/index.mjs
@@ -0,0 +1,3 @@
+export * from './logOut.mjs';
+export * from './renderProfile.mjs';
+export * from './updateNavLinks.mjs';
---
+++ src/js/api/items/index.mjs
@@ -0,0 +1,4 @@
+export * from './create-post.mjs';
+export * from './delete-post.mjs';
+export * from './get-posts.mjs';
+export * from './update-post.mjs';

0bd4f08581952859833f43343d99730523d20530
S10ANDK <91675991+S10ANDK@users.noreply.github.com>
2022-11-29 18:30:18 +0100

    changed url for sorting

--- src/js/api/constants.mjs
+++ src/js/api/constants.mjs
@@ -3,4 +3,4 @@ export const API_DATABASE = '/api/v1';
 export const API_DATABASE_AUCTION = '/auction';
 export const API_AUCTION_URL = `${API_URL}${API_DATABASE}${API_DATABASE_AUCTION}`;
 export const auctionURLDesc = `${API_URL}${API_DATABASE}${API_DATABASE_AUCTION}/listings?sort=created&sortOrder=desc`;
-export const auctionURLAsc = `${API_URL}${API_DATABASE}${API_DATABASE_AUCTION}/listings?sort=title&sortOrder=asc`;
+export const auctionURLAsc = `${API_URL}${API_DATABASE}${API_DATABASE_AUCTION}/listings?sort=created&sortOrder=asc`;

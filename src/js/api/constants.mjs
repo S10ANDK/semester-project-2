797025e4a32861d7125460499c98069fdf17df8a
S10ANDK <91675991+S10ANDK@users.noreply.github.com>
2022-11-29 16:31:05 +0100

    few tweaks and corrected URL's

--- src/js/api/constants.mjs
+++ src/js/api/constants.mjs
@@ -2,5 +2,5 @@ export const API_URL = 'https://nf-api.onrender.com';
 export const API_DATABASE = '/api/v1';
 export const API_DATABASE_AUCTION = '/auction';
 export const API_AUCTION_URL = `${API_URL}${API_DATABASE}${API_DATABASE_AUCTION}`;
-export const auctionURLDesc = `${API_URL}${API_DATABASE}${API_DATABASE_AUCTION}?sort=created&sortOrder=desc`;
-export const auctionURLAsc = `${API_URL}${API_DATABASE}${API_DATABASE_AUCTION}?sort=title&sortOrder=asc`;
+export const auctionURLDesc = `${API_URL}${API_DATABASE}${API_DATABASE_AUCTION}/listings?sort=created&sortOrder=desc`;
+export const auctionURLAsc = `${API_URL}${API_DATABASE}${API_DATABASE_AUCTION}/listings?sort=title&sortOrder=asc`;
--- src/js/index.mjs
+++ src/js/index.mjs
@@ -2,3 +2,5 @@ import * as listeners from './handlers/index.mjs';
 import {} from './components/index.mjs';
 
 listeners.getItemsList();
+listeners.filterAscending();
+listeners.filterDescending();

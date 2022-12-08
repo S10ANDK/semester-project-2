b492b70fc54fc6c58210fa23e25fa3aef4daa302
S10ANDK <91675991+S10ANDK@users.noreply.github.com>
2022-12-08 19:01:34 +0100

    added UX and error handling for create item page

--- src/js/api/items/create-item.mjs
+++ src/js/api/items/create-item.mjs
@@ -1,10 +1,9 @@
 import { API_AUCTION_URL } from '../constants.mjs';
+import { errorMessage } from '../../components/displayError.mjs';
 
 /**
  * Passes data in order to create a post
  */
-
-
 
  export async function createItem(itemData) {
     const method = 'POST'
@@ -20,34 +19,44 @@ import { API_AUCTION_URL } from '../constants.mjs';
     body: JSON.stringify(itemData)
   })
 
-  const result =  await response.json();
-//   const statusCodeResponse = result.status;
-  console.log(result);
-//   console.log(statusCodeResponse);
+  const formContainer = document.querySelector('#createItemContainer');
+  const form = document.querySelector('#createItemForm');
 
-//   if (statusCodeResponse === 200 ||
-//     statusCodeResponse === 201 ||
-//     statusCodeResponse === 204) {
-//     alert('success');
-//   } else {
-//     alert('failed')
-//   }
+  if (response.ok ) {
+    form.style.display = 'none';
+    form.classList.remove('d-flex');
+    const success = document.createElement('p');
+    const successText = document.createTextNode(
+      'Success!'
+    );
+
+    success.appendChild(successText);
+    formContainer.appendChild(success);
+    formContainer.classList.add(
+      'text-center',
+      'mw-600',
+      'mx-auto',
+      'pt-3',
+      'fade-in');
+
+      const icon = document.createElement('img');
+      icon.setAttribute('src', '/assets/success_icon.png');
+      icon.classList.add('success_icon', 'text-center', 'mx-auto', 'pt-3');
+      formContainer.appendChild(icon);
+
+    setTimeout(function timer() {
+      window.location.href = '/';
+    }, 2000);
+  } else {
+    console.log('An error has occured');
+    console.log(response.status);
+    form.style.display = 'none';
+    form.classList.remove('d-flex');
+    formContainer.classList.add('fade-in');
+    formContainer.innerHTML = errorMessage(
+      'An error has occured. Please refresh the page and try again.'
+    );
+  }
+
+  return await response.json();
 }
-
-// export async function createItem(itemData) {
-//     const token = localStorage.getItem('accessToken');
-//     const options = {
-//     method: 'POST',
-//     headers: {
-//         'Content-Type': 'application/json',
-//         'Authorization': `Bearer ${token}`,
-//       },
-//       body: JSON.stringify(itemData),
-//     };
-
-//   const createItemUrl = API_AUCTION_URL + '/listings';
-//   const response = await fetch(createItemUrl,
-//     options)
-
-//   return await response.json();
-// }
